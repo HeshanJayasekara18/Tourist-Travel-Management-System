7b6c7286e5d9b4723c016b4db3877af5171d823b
HeshanJayasekara18 <heshanjayasekara18@gmail.com>
2025-04-26 16:24:02 +0530

    bussiness reg

--- backend/route/BussinessRegisterRoute.js
+++ backend/route/BussinessRegisterRoute.js
@@ -2,16 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {
     register,
-<<<<<<< HEAD
-    getBussinessDetails
-   } = require('../controller/BussinessRegisterController');
-
-   
- router.post('/', register); 
- router.get('/', getBussinessDetails);    
-
- module.exports = router;
-=======
     getBussinessDetails,
     loginBussiness
   } = require('../controller/BussinessRegisterController');
@@ -22,4 +12,3 @@ router.get('/', getBussinessDetails);
 router.post('/login', loginBussiness);
 
 module.exports = router;
->>>>>>> 7de07865f64e44dc5091f0fb6eb98dd878fc4d57
